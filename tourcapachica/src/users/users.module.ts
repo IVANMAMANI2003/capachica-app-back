06b6480858d9388a8bf396c460f197010a406e48
IVANMAMANI2003 <82121615+IVANMAMANI2003@users.noreply.github.com>
2025-05-16 00:37:29 -0500

    5 update - recover password

--- tourcapachica/src/users/users.module.ts
+++ tourcapachica/src/users/users.module.ts
@@ -4,6 +4,7 @@ import { UsersController } from './users.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { SupabaseModule } from '../supabase/supabase.module';
 import { MailerModule, MailerService } from '@nestjs-modules/mailer';
+import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
 
 
 
@@ -15,3 +16,22 @@ import { MailerModule, MailerService } from '@nestjs-modules/mailer';
 })
 export class UsersModule {}
 
+MailerModule.forRoot({
+  transport: {
+    service: 'gmail',
+    auth: {
+      user: 'tu-correo@gmail.com',
+      pass: 'tu-contraseña-o-app-password',
+    },
+  },
+  defaults: {
+    from: '"Tu App" <tu-correo@gmail.com>',
+  },
+  template: {
+    dir: __dirname + '/templates',
+    adapter: new HandlebarsAdapter(),
+    options: {
+      strict: true,
+    },
+  },
+})
